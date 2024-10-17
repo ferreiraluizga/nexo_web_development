65ca721a0eb03359e4235addb15d81ccb8c2c16f
BeccaDev <130665887+RebecadeMouraMendes@users.noreply.github.com>
2024-10-17 08:20:39 -0300

    feat: models do dashboard

---
+++ src/model/categoria.php
@@ -0,0 +1,86 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/nexo/controller/connect.php'; //Importação única do arquivo, se existente
+
+class Categoria{ //Classe publica
+    private $Cod_Categoria; //Variáveis 
+    private $nome_categoria;
+    private $desc_categoria;
+
+    //Gets e Sets, encapsulamento das variáveis 
+    public function getCod_Categoria(){ 
+        return $this->Cod_Categoria;
+    }
+    public function setCod_Categoria($Cod_Categoria){
+        $this->Cod_Categoria = $Cod_Categoria;
+    }
+
+    public function getnome_categoria(){
+        return $this->nome_categoria;
+    }
+    public function setnome_categoria($nome_categoria){
+        $this->nome_categoria = $nome_categoria;
+    }
+
+    public function getdesc_categoria(){
+        return $this->desc_categoria;
+    }
+    public function setdesc_categoria($desc_categoria){
+        $this->desc_categoria = $desc_categoria;
+    }
+
+    public function __construct(){ //Método construtor
+        $this->connect = new Connect(); //Instancia do objeto da classe conexão
+    }
+
+    //Método insirir faz a conexão com o banco de dados
+    public function inserir(){
+        $sql = "INSERT INTO categoria_produto (`Nome_Categoria`,`Desc_Categoria`) VALUES (?,?)"; //Declaração para o sql, de forma indefinida
+        $stmt = $this->connect->getConexao()->prepare($sql); // stmt acessa conexão, realiza o método e utiliza o prepare
+        $stmt->bind_param('ss', $this->nome_categoria, $this->desc_categoria); //Define os parametros no caso do s, String
+        return $stmt->execute(); //Retorna e executa
+    }
+
+    public function listar(){ //Método listar
+        $sql = "SELECT * FROM categoria_produto"; //Declaração para sql, utilizando select
+        $stmt = $this->connect->getConexao()->prepare($sql); //Acessa e busca a conexão, prepara o banco de dados para receber a declaração na variável sql 
+        $stmt->execute(); //execução do armazenamento stmt
+        $result = $stmt->get_result(); // Busca os resultados do select
+        $categorias = []; // Vetor pessoas
+        while($categoria = $result->fetch_assoc()){ //Enquanto existir informações, retorna uma matriz onde armazena os registros 
+            $categorias[] = $categoria; //Atribuição no vetor
+        }
+        return $categorias; //Retorna o vetor
+    }
+
+
+    public function atualizar($Cod_Categoria){
+        $sql = "UPDATE categoria_produto SET Nome_Categoria = ?, Desc_Categoria = ? WHERE Cod_Categoria = ?"; //Declaração para o sql, de forma indefinida
+        $stmt = $this->connect->getConexao()->prepare($sql); // stmt acessa conexão, realiza o método e utiliza o prepare
+        $stmt->bind_param('ssi', $this->nome_categoria, $this->desc_categoria, $Cod_Categoria); //Define os parametros no caso do s, String
+        return $stmt->execute(); //Retorna e executa
+    }
+
+    public function buscarPorCod_Categoria($Cod_Categoria){  //Método buscarPorId
+        $sql = "SELECT * FROM categoria_produto WHERE Cod_Categoria = ?"; //Declaração para sql, utilizando select e where
+        $stmt = $this->connect->getConexao()->prepare($sql);  //Acessa e busca a conexão, prepara o banco de dados para receber a declaração na variável sql 
+        $stmt->bind_param('i', $Cod_Categoria); //Define os parametros 
+        $stmt->execute(); //execução do armazenamento stmt
+        $result = $stmt->get_result();  // Busca os resultados do select
+        return $result->fetch_assoc(); //Enquanto existir informações, retorna uma matriz onde armazena os registros 
+    }
+
+
+    public function buscar($search) {
+        // Prepare uma consulta SQL com um placeholder
+        $sql = "SELECT * FROM categoria_produto WHERE Nome_Categoria LIKE '%$search%' OR Cod_Categoria LIKE '%$search%'";
+        $stmt = $this->connect->getConexao()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result(); // Busca os resultados do select
+        $listas = []; // Vetor  
+        while($lista = $result->fetch_assoc()){ //Enquanto existir informações, retorna uma matriz onde armazena os registros 
+            $listas[] = $lista; //Atribuição no vetor
+        }
+        return $listas; //Retorna o vetor
+} }
+
+?>
---
+++ src/model/marca.php
@@ -0,0 +1,77 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/nexo/controller/connect.php'; //Importação única do arquivo, se existente
+
+class Marca{ //Classe publica
+    private $Cod_Marca; //Variáveis 
+    private $nome_marca;
+
+    //Gets e Sets, encapsulamento das variáveis 
+    public function getCod_Marca(){ 
+        return $this->Cod_Marca;
+    }
+    public function setCod_Marca($Cod_Marca){
+        $this->Cod_Marca = $Cod_Marca;
+    }
+
+    public function getnome_marca(){
+        return $this->nome_marca;
+    }
+    public function setnome_marca($nome_marca){
+        $this->nome_marca = $nome_marca;
+    }
+
+    public function __construct(){ //Método construtor
+        $this->connect = new Connect(); //Instancia do objeto da classe conexão
+    }
+
+    //Método insirir faz a conexão com o banco de dados
+    public function inserir(){
+        $sql = "INSERT INTO marca (`Nome_Marca`) VALUES (?)"; //Declaração para o sql, de forma indefinida
+        $stmt = $this->connect->getConexao()->prepare($sql); // stmt acessa conexão, realiza o método e utiliza o prepare
+        $stmt->bind_param('s', $this->nome_marca); //Define os parametros no caso do s, String
+        return $stmt->execute(); //Retorna e executa
+    }
+
+    public function listar(){ //Método listar
+        $sql = "SELECT * FROM marca"; //Declaração para sql, utilizando select
+        $stmt = $this->connect->getConexao()->prepare($sql); //Acessa e busca a conexão, prepara o banco de dados para receber a declaração na variável sql 
+        $stmt->execute(); //execução do armazenamento stmt
+        $result = $stmt->get_result(); // Busca os resultados do select
+        $marcas = []; // Vetor pessoas
+        while($marca = $result->fetch_assoc()){ //Enquanto existir informações, retorna uma matriz onde armazena os registros 
+            $marcas[] = $marca; //Atribuição no vetor
+        }
+        return $marcas; //Retorna o vetor
+    }
+
+
+    public function atualizar($Cod_Marca){
+        $sql = "UPDATE marca SET Nome_Marca = ? WHERE Cod_Marca = ?"; //Declaração para o sql, de forma indefinida
+        $stmt = $this->connect->getConexao()->prepare($sql); // stmt acessa conexão, realiza o método e utiliza o prepare
+        $stmt->bind_param('si', $this->nome_marca, $Cod_Marca); //Define os parametros no caso do s, String
+        return $stmt->execute(); //Retorna e executa
+    }
+
+    public function buscarPorCod_Marca($Cod_Marca){  //Método buscarPorId
+        $sql = "SELECT * FROM marca WHERE Cod_Marca = ?"; //Declaração para sql, utilizando select e where
+        $stmt = $this->connect->getConexao()->prepare($sql);  //Acessa e busca a conexão, prepara o banco de dados para receber a declaração na variável sql 
+        $stmt->bind_param('i', $Cod_Marca); //Define os parametros 
+        $stmt->execute(); //execução do armazenamento stmt
+        $result = $stmt->get_result();  // Busca os resultados do select
+        return $result->fetch_assoc(); //Enquanto existir informações, retorna uma matriz onde armazena os registros 
+    }
+
+    public function buscar($search) {
+        // Prepare uma consulta SQL com um placeholder
+        $sql = "SELECT * FROM marca WHERE Nome_Marca LIKE '%$search%' OR Cod_Marca LIKE '%$search%'";
+        $stmt = $this->connect->getConexao()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result(); // Busca os resultados do select
+        $listas = []; // Vetor  
+        while($lista = $result->fetch_assoc()){ //Enquanto existir informações, retorna uma matriz onde armazena os registros 
+            $listas[] = $lista; //Atribuição no vetor
+        }
+        return $listas; //Retorna o vetor
+} }
+
+?>
